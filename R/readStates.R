6ee8d23b5e54c202396a1704e43ca9811479cf5f
abhimishr <A.Mishra@cgiar.org>
2023-06-12 16:49:42 -0400

    initial build

---
+++ R/readStates.R
@@ -0,0 +1,42 @@
+#' readStates
+#'
+#' @return Cell area for LUHv2 land use types on 0.25 degree grid
+#' @importFrom ncdf4 nc_open
+#' @importFrom terra rast time
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' readStates()
+#' }
+#' @author Abhijeet Mishra
+readStates <- function() {
+    # Basic ----
+    ncpath <- getOption("luhv2_loc")
+    ncname <- "states"
+    ncfname <- paste(ncpath, ncname, ".nc", sep = "")
+
+    area_name <- paste(ncpath, "staticData_quarterdeg", ".nc", sep = "")
+
+    varnames <- names(nc_open(ncfname)[["var"]])
+
+    # TERRA ----
+
+    raster_list <- list()
+    exclude <- c("lat_bounds", "lon_bounds", "secma", "secmb")
+    for (i in varnames[!(varnames %in% exclude)]){
+        raster_list[i] <- rast(ncfname, subds = i)[[1166]]
+        message(i, " ---> ", time(raster_list[[i]]))
+    }
+
+    raster_stack <- rast(raster_list)
+
+    cell_area <-  rast(area_name, subds = "carea")
+
+    land_use_area <- raster_stack * cell_area
+    land_use_area <- land_use_area * 1e6 #km2 to m2
+    land_use_area <- land_use_area / 1e4 #m2 to ha
+    land_use_area <- land_use_area / 1e6 #ha to mha
+
+    return(land_use_area)
+}
